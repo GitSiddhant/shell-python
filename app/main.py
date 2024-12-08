cf5301472d6440359ec760e822e34db7f07f18c8
GitSiddhant <siddhantjain778@gmail.com>
2024-12-08 19:03:51 +0530

    Run a program-3

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ def handle_type(args):
     elif executable := locate_executable(args[0]):
         print(f"{args[0]} is {executable}")
     else:
-        print(f"{args[0]} not found")
+        print(f"{args[0]}: not found")
 builtins = {
     "exit": handle_exit,
     "echo": handle_echo,
