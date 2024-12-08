3e6bf928e6d67480e2f1fc85c984473315d7cec5
GitSiddhant <siddhantjain778@gmail.com>
2024-12-08 19:19:29 +0530

    Run a program-4

--- app/main.py
+++ app/main.py
@@ -8,22 +8,22 @@ def locate_executable(command) -> Optional[str]:
         file_path = os.path.join(directory, command)
         if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
             return file_path
-def handle_exit(args):
-    sys.exit(int(args[0]) if args else 0)
-def handle_echo(args):
-    print(" ".join(args))
-def handle_type(args):
-    if args[0] in builtins:
-        print(f"{args[0]} is a shell builtin")
-    elif executable := locate_executable(args[0]):
-        print(f"{args[0]} is {executable}")
-    else:
-        print(f"{args[0]}: not found")
-builtins = {
-    "exit": handle_exit,
-    "echo": handle_echo,
-    "type": handle_type,
-}
+# def handle_exit(args):
+#     sys.exit(int(args[0]) if args else 0)
+# def handle_echo(args):
+#     print(" ".join(args))
+# def handle_type(args):
+#     if args[0] in builtins:
+#         print(f"{args[0]} is a shell builtin")
+#     elif executable := locate_executable(args[0]):
+#         print(f"{args[0]} is {executable}")
+#     else:
+#         print(f"{args[0]}: not found")
+# builtins = {
+#     "exit": handle_exit,
+#     "echo": handle_echo,
+#     "type": handle_type,
+# }
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
@@ -59,7 +59,7 @@ def main():
 
             elif(executable := locate_executable(command[0])):
                 #print(executable)
-                subprocess.run(executable,command[1:])
+                subprocess.run([executable,command[1:]])
             # elif(command[0].startswith("program")):
             #     print("Hello "+command[1+"!"],end = " ")
             #     execute_program(command[0])
